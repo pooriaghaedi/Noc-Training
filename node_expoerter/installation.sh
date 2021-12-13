b3691be7c40da77249a502a7bd5e98c1d84da205
Poori <poori@MacBook-Pro.local>
2021-12-13 19:41:00 +0330

    initial commit

--- node_expoerter/installation.sh
+++ node_expoerter/installation.sh
@@ -6,7 +6,7 @@ tar xvf node_exporter-1.3.1.linux-amd64.tar.gz
 cd node_exporter-1.3.1.linux-amd64/
 mv node_exporter /usr/local/bin/
 useradd -rs /bin/false node_exporter
-chown root:root -R /usr/local/bin/node_exporter
+chown node_exporter:node_exporter -R /usr/local/bin/node_exporter
 vim /etc/systemd/system/node_exporter.service
 systemctl daemon-reload
 systemctl start node_exporter.service
